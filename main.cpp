e943cdbb7b51b44904d033eb8aba54ba447d3600
Pavel <p.a.zhdanov@mai.ru>
2020-10-29 00:04:21 +0300

    Merge template file into main.cpp, add command parsing

--- main.cpp
+++ main.cpp
@@ -1,5 +1,6 @@
 #include <string>
 #include <iostream>
+#include <sstream>
 #include <iomanip>
 
 using namespace std;
@@ -25,17 +26,26 @@ struct Day {
 	int value;
 };
 
-struct Date {
-		Date(){}
-		Date(const Year& new_year, const Month& new_month, const Day& new_day){
-			year = new_year.value;	
-			month = new_month.value;
-			day = new_day.value;
-		}
-		int year;
-		int month;
-		int day;
+class Date {
+public:
+	Date(){}
+	Date(const Year& new_year, 
+	     const Month& new_month, 
+	     const Day& new_day){
+		year = new_year.value;	
+		month = new_month.value;
+		day = new_day.value;
+	}
+	int GetYear() const {return year;};
+	int GetMonth() const {return month;};
+	int GetDay() const {return day;};
+private:
+	int year;
+	int month;
+	int day;
 };
+
+bool operator<(const Date& lhs, const Date& rhs);
 
 istream& operator>>(istream &stream, Date &date)
 {
@@ -54,19 +64,50 @@ istream& operator>>(istream &stream, Date &date)
 ostream& operator<<(ostream &stream, const Date &date)
 {
 	char delimeter = '-';
-	stream << setfill('0') << setw(4) << date.year;
+	stream << setfill('0') << setw(4) << date.GetYear();
 	stream << delimeter;
-	stream << setfill('0') << setw(2) << date.month;
+	stream << setfill('0') << setw(2) << date.GetMonth();
 	stream << delimeter;
-	stream << setfill('0') << setw(2) << date.day;
+	stream << setfill('0') << setw(2) << date.GetDay();
 	return stream;
 }
-int main(void)
-{
-	Date date(Year(2020), Month(12), Day(31));
-	Date date1;
-	Date date2;
-	cin >> date1 >> date2;
-	cout << date1 << endl << date2 << endl;
+// Реализуйте функции и методы классов и при необходимости добавьте свои
+
+class Database {
+public:
+  void AddEvent(const Date& date, const string& event);
+  bool DeleteEvent(const Date& date, const string& event);
+  int  DeleteDate(const Date& date);
+
+  ///* ??? */ Find(const Date& date) const;
+  
+  void Print() const;
+};
+
+int main() {
+	//Date date1;
+	//Date date2;
+	//cin >> date1 >> date2;
+	//cout << date1 << endl << date2 << endl;
+	//Database db;
+
+	string command;
+	while (getline(cin, command)) {
+		stringstream input(command);
+		if (command == "Add"){
+			cout << "Add command" << endl;
+		} else if (command == "Del"){
+			cout << "Del command" << endl;
+		} else if (command == "Find"){
+			cout << "Find command" << endl;
+		} else if (command == "Print"){
+			cout << "Print command" << endl;
+		} else {
+			cout << "Unknown command" << endl;
+		}
+
+		// Считайте команды с потока ввода и обработайте каждую
+	}
+
 	return 0;
 }
